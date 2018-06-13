e1db3dffa57031815738d781cfdbdb7e1f74f7b9
benjaphon <benjaphon.mail@gmail.com>
2018-06-13 22:09:06 +0700

    add files

---
+++ assets/library/security.php
@@ -0,0 +1,41 @@
+<?php
+
+class security {
+
+    private function setadmin() {
+        if ($_SESSION[_ss . 'levelaccess'] == 'admin') {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+    private function setuser() {
+        if ($_SESSION[_ss . 'levelaccess'] == 'user') {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+    public function check($type = "user") {
+        if ($type == "admin") {
+            $check = $this->setadmin();
+            $dir = "back";
+        } else {
+            $check = $this->setuser();
+            $dir = "front";
+        }
+
+        if ($check == FALSE) {
+            header("location:" . base_url() . "/" . $dir . "/user/login");
+        }
+    }
+
+    public function check_login() {
+        if(!isset($_SESSION[_ss . 'id'])){
+            header("location:" . base_url() . "/back/user/login");
+        }
+    }
+
+}
