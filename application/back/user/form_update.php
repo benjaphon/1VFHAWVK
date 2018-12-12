aa04debcd321abf0ae8adf61e753ad20043e22c4
benjaphon <benjaphon.mail@gmail.com>
2018-12-12 13:11:53 +0700

    user form update change

--- application/back/user/form_update.php
+++ application/back/user/form_update.php
@@ -1,17 +1,17 @@
 <?php
-$db = new database();
 
-$password = salt_pass($_POST['new_password']);
-$value_user = array(
-    "username" => trim($_POST['username']),
-    "password" => $password,
-    "address" => trim($_POST['address']),
-    "role" => trim($_POST['role'])
-);
-$con_user = "id='{$_GET['id']}'";
-$query_user = $db->update("users", $value_user, $con_user);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    
+    $db = new database();
+    $password = salt_pass($_POST['new_password']);
+    $value = array(
+        "username" => trim($_POST['username']),
+        "password" => $password,
+        "user_type" => trim($_POST['user_type'])
+    );
+    $query = $db->update("subject", $values, "id={$_POST['id']}");
 
-if($query_user == TRUE){
-    header("location:" . $baseUrl . "/back/user");
+    if ($query) {
+        header("location:" . $baseUrl . "/back/user");
+    }
 }
-mysql_close();
