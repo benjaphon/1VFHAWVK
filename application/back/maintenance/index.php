a7eafaa5430032a2d17069b286f9fdec0813981b
Benjaphon Niramit <benjaphon.work@gmail.com>
2020-09-05 11:07:11 +0700

    update

--- application/back/maintenance/index.php
+++ application/back/maintenance/index.php
@@ -1,19 +1,19 @@
 <!doctype html>
 <title>Site Maintenance</title>
 <style>
-  body { text-align: center; padding: 150px; }
+  body { text-align: center; padding: 10%; }
   h1 { font-size: 50px; }
   body { font: 20px Helvetica, sans-serif; color: #333; }
-  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  article { display: block; text-align: left; max-width: 800px; margin: 0 auto; }
   a { color: #dc8100; text-decoration: none; }
   a:hover { color: #333; text-decoration: none; }
 </style>
 
 <article>
-    <h1>We&rsquo;ll be back soon!</h1>
+    <h1>เราจะกลับมาเร็ว ๆ นี้!</h1>
     <div>
-        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. We&rsquo;ll be back online shortly!</p>
-        <p>&mdash; Borbaimai Soft</p>
-        <p>Go Back to <a href="/back/user/login">Login Page</a></p>
+        <p>ขออภัยในความไม่สะดวก แต่เรากำลังดำเนินการบำรุงรักษาบางส่วนในขณะนี้ หากคุณต้องการความช่วยเหลือโปรดติดต่อแอดมิน เราจะกลับมาออนไลน์อีกครั้งในไม่ช้า!</p>
+        <p>&mdash; บอใบไม้ ซอฟท์</p>
+        <p>กลับสู่หน้า <a href="/back/user/login">ล็อคอิน</a></p>
     </div>
 </article>
